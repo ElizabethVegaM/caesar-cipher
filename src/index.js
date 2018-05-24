e1b890a1f3fe0139b814da05340cb035d70b645d
RavAlmasy <eb.vega.m@gmail.com>
2018-05-24 17:31:09 -0400

    agregada funcionalidad de botones, cambio de input por textarea y algunas cosas específicas referente al estilo

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
-/* Acá va tu código */
+window.onload = () => {
+  const welcomeBox = document.getElementById('welcomeBox');
+  const encodeBtn = document.getElementById('encodeBtn');
+  const decodeBtn = document.getElementById('decodeBtn');
+  const toMainBtn = document.getElementById('toMain');
+
+  encodeBtn.addEventListener('click', () => {
+    document.getElementById('encodeBox').style.display = 'block';
+    welcomeBox.style.display = 'none';
+  });
+
+  decodeBtn.addEventListener('click', () => {
+    document.getElementById('decodeBox').style.display = 'block';
+    welcomeBox.style.display = 'none';
+  });
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  toMainBtn.addEventListener('click', () => {
+    location.reload();
+    });
+
+}
+
