9f625415f4dc736bae10190630528c4a0fe1d9bd
RavAlmasy <eb.vega.m@gmail.com>
2018-06-01 09:29:50 -0400

    añadida las carpeta docs con los elementos necesarios para hacer deploy

---
+++ docs/cipher.js
@@ -0,0 +1,39 @@
+window.cipher = {
+  encode: (txtToCode, offsetCode) => {
+    console.log(`Ingresando en Cipher Code`);
+    console.log(`La frase a cifrar es ${txtToCode} y se moverá ${offsetCode} carácteres`);
+    let encodedMsg = '';
+    for (let i = 0; i < txtToCode.length; i++) {
+      if (txtToCode.charCodeAt(i) >= 97 && txtToCode.charCodeAt(i) <= 122) {
+        let cipher = (txtToCode.charCodeAt(i) - 97 + parseInt(offsetCode)) % 26 + 97;
+        console.log(cipher);
+        encodedMsg += String.fromCharCode(cipher);
+        console.log(encodedMsg);
+      } else if (txtToCode.charCodeAt(i) >= 65 && txtToCode.charCodeAt(i) <= 90) {
+        let cipher = (txtToCode.charCodeAt(i) - 65 + parseInt(offsetCode)) % 26 + 65;
+        console.log(cipher);
+        encodedMsg += String.fromCharCode(cipher);
+        console.log(encodedMsg);
+      } else if (txtToCode.charCodeAt(i) === 32) {
+        cipher = ' ';
+        encodedMsg += ' ';
+      };
+    };
+    return encodedMsg;
+  },
+  decode: (txtToDecode, offsetDecode) => {
+    console.log(`Ingresando en Decipher Code`);
+    console.log(`La frase a descifrar es ${txtToDecode} y se moverá ${offsetDecode} carácteres`);
+    let decodedMsg = '';
+    for (let i = 0; i < txtToDecode.length; i++) {
+      let cipher = (txtToDecode.charCodeAt(i) + 65 - parseInt(offsetDecode)) % 26 + 65;
+      if (txtToDecode.charCodeAt(i) === 32) {
+        cipher = ' ';
+        decodedMsg += ' ';
+      }
+      console.log(cipher);
+      decodedMsg += String.fromCharCode(cipher);
+    };
+    return decodedMsg;
+  }
+};
---
+++ docs/index.js
@@ -0,0 +1,52 @@
+window.onload = () => {
+  const welcomeBox = document.getElementById('welcomeBox');
+  const encodeBtn = document.getElementById('encodeBtn');
+  const decodeBtn = document.getElementById('decodeBtn');
+  const toMainBtn = document.getElementById('toMain');
+  const cipherBtn = document.getElementById('cipherBtn');
+  const decipherBtn = document.getElementById('decipherBtn');
+  const decodedBox = document.getElementById('decodedBox');
+
+  //Pop-up que muestra las instrucciones
+  document.getElementById('popUp').addEventListener('click', () => {
+    var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+  });
+
+  //Para mostrar la caja con el proceso de cifrado
+  encodeBtn.addEventListener('click', () => {
+    document.getElementById('encodeBox').style.display = 'block';
+    welcomeBox.style.display = 'none';
+  });
+
+  //Para mostrar el proceso de descifrado
+  decodeBtn.addEventListener('click', () => {
+    document.getElementById('decodeBox').style.display = 'block';
+    welcomeBox.style.display = 'none';
+  });
+
+  //Cifrado
+  cipherBtn.addEventListener('click', () => {
+    let txtToCode = document.getElementById('txtToCode').value;
+    let offsetCode = document.getElementById('numToMove').value;
+    let readyMsg = window.cipher.encode(txtToCode, offsetCode);
+    document.getElementById('encodedMsg').value = readyMsg;
+    document.getElementById('encodedBox').style.display = 'block';
+  });
+
+
+  //Descifrado
+  decipherBtn.addEventListener('click', () => {
+    let txtToDecode = document.getElementById('txtToDecode').value.toUpperCase();
+    let offsetDecode = document.getElementById('numToMoveDec').value;
+    let readyMsg = window.cipher.decode(txtToDecode, offsetDecode);
+    document.getElementById('decodedMsg').value = readyMsg;
+    document.getElementById('decodedBox').style.display = 'block';
+  });
+
+  //Para "volver" a la página principal
+  toMainBtn.addEventListener('click', () => {
+    location.reload();
+  });
+
+};
